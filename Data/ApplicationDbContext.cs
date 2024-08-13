b3699b40a8361460ba8b574edbce0912a0e25c6f
OLEMUKAN <olemukan4@gmail.com>
2024-08-13 18:17:17 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TruckLoadingApp.Models;
+
+namespace TruckLoadingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+    }
+}
